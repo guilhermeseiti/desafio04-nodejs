de116874102f5cacc8434dfa821750d0e5f0b5fe
Guilherme Seiti <gseitihomma@gmail.com>
2021-05-16 15:41:33 -0300

    created useCases, repositories and controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,21 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const userList = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(userList);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
